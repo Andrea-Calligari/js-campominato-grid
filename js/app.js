7884bbbeaa171e5b9e1617a576a644e3eb0110bc
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-03-13 20:33:24 +0100

    ADD: some style -and header

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 const gridDOMElement = document.querySelector('.grid');
-const selectDOMElement = document.getElementById('')
+const buttonDOMElement = document.getElementById('button')
+console.log(buttonDOMElement);
 const size = 10;
 const numOfCellElement = size ** 2 // potenza di 2 => 8 * 8 
 
-for (let i = 0; i < numOfCellElement; i++) {
+buttonDOMElement.addEventListener('click', function () {
+
+  for (let i = 0; i < numOfCellElement; i++) {
     const num = i + 1;
 
     const cellElement = document.createElement('div');
@@ -11,20 +14,22 @@ for (let i = 0; i < numOfCellElement; i++) {
     cellElement.innerHTML = num
 
     gridDOMElement.append(cellElement)
-
+    
+    
     cellElement.addEventListener('click', function () {
-
-        console.log('click casell numero:', num);
-
-
-        if (cellElement.classList.contains('bg-dark') === true) {
-            cellElement.classList.remove('bg-dark');
-        } else {
-            cellElement.classList.add('bg-dark');
-        }
-
-        // cellElement.classList.toggle('bg-dark'); // questo fa esattamente cio che fa l' if sopra
-
+      
+      console.log('click casell numero:', num);
+      
+      
+      if (cellElement.classList.contains('bg-dark') === true) {
+        cellElement.classList.remove('bg-dark');
+      } else {
+        cellElement.classList.add('bg-dark');
+      }
+      
+      // cellElement.classList.toggle('bg-dark'); // questo fa esattamente cio che fa l' if sopra
+      
     });
-}
-
+  
+  }
+  })
