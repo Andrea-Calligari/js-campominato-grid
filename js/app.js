86e21ca0be64195d9e54c95fa1ac1a013b4f3795
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-03-13 16:23:28 +0100

    htmlstructure-css-and-js

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const gridDOMElement = document.querySelector('.grid');
+
+const size = 10;
+const numOfCellElement = size ** 2 // potenza di 2 => 8 * 8 
+
+for (let i = 0; i < numOfCellElement; i++) {
+    const num = i + 1;
+
+    const cellElement = document.createElement('div');
+    cellElement.className = 'cell';
+    cellElement.innerHTML = num
+
+    gridDOMElement.append(cellElement)
+
+    cellElement.addEventListener('click', function (){
+
+        console.log('click casell numero:', num);
+
+        cellElement.classList.add('bg-dark')
+
+        
+        
+    });
+    cellElement.addEventListener('dblclick', function (){
+
+        
+
+        cellElement.classList.remove('bg-dark');
+
+        
+    });
+
+
+
+
+
+
+
+
+
+}
+
